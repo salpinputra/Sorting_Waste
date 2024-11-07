90e4885f43650ab3cf316cc3ed9df5c8b2bc10cc
salpinputra <salpinputra45@gmail.com>
2024-11-07 17:01:07 +0700

    menambahkan asset baru

---
+++ Assets/Project/Script/SplashScreen.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string halamanSelanjutnya;
+    public int waktuTunggu;
+    void Awake()
+
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PindahHalaman());
+    }
+
+    IEnumerator PindahHalaman()
+    {
+        yield return new WaitForSeconds(waktuTunggu);
+        SceneManager.LoadScene(halamanSelanjutnya);
+    }
+}
